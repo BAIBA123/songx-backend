389b667d4e002d20048080d1f24aeb6bedfe314e
songxin <20013887@suning.com>
2020-07-27 09:42:03 +0800

    测试ui

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,8 +9,9 @@ export default function Header(props: HeaderProps) {
   const {setCollapsed, collapsed} = props
 
   return (
-    <div className={`h-12 bg-white shadow absolute top-0 right-0 transform duration-300 ${collapsed ? 'left-20' : 'left-56'}`}>
+    <div className={`flex h-12 bg-white shadow absolute top-0 right-0 transform duration-300 ${collapsed ? 'left-20' : 'left-56'}`}>
       <button onClick={() => setCollapsed(!collapsed)}>toggle</button>
+      <p>111</p>
     </div>
   )
 }
