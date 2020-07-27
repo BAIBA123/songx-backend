422e5fcbf781a1a9b6ac8ade0e553251e652aac6
songxin <20013887@suning.com>
2020-07-27 09:45:00 +0800

    header center

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,9 +9,8 @@ export default function Header(props: HeaderProps) {
   const {setCollapsed, collapsed} = props
 
   return (
-    <div className={`flex h-12 bg-white shadow absolute top-0 right-0 transform duration-300 ${collapsed ? 'left-20' : 'left-56'}`}>
+    <div className={`flex items-center px-6 h-12 bg-white shadow absolute top-0 right-0 transform duration-300 ${collapsed ? 'left-20' : 'left-56'}`}>
       <button onClick={() => setCollapsed(!collapsed)}>toggle</button>
-      <p>111</p>
     </div>
   )
 }
